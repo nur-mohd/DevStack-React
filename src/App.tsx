5d56114c6100850e434a606f15ee8fd5b193f774
Nur Mohammad <nurmohammad6465@gmail.com>
2026-09-11 21:15:37 +0600

    Initial Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-red-600">Hi... Nur</h1>
+      <button className="btn btn-secondary">Secondary</button>
+    </>
+  )
+}
+
+export default App
